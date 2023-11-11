ce9e4257c41f444e07a3a55b95c522b7c9c798d7
CaiooGomes <gomescaio898@gmail.com>
2023-11-11 16:33:29 -0300

    finalizado

---
+++ js/index.js
@@ -0,0 +1,16 @@
+function apagarimagens(){
+    var imagens = document.getElementById("imagens")
+    imagens.innerHTML = ""
+}
+function telaescura(){
+    var telacinza = document.getElementById("telaescura")
+    telacinza.innerHTML = "<div class='telaescura'></div>"
+}
+function tirarbotao(){
+    var removerbotao = document.getElementById("tirarbotao")
+    removerbotao.innerHTML = "<div> </div>"
+}
+function textoespecialf(){
+    var texto = document.getElementById("textoespecial")
+    texto.innerHTML = "<div class='textoespecial'>Oi Bianca, tudo bem? <br> Resolvi deixar um recado de um jeito diferente, pra sair um pouco do padrão, né? Gosto muito de você e resolvi fazer algo especial. Só quero que você saiba que você foi um acaso que eu não esperava esse ano, uma pessoa gentil, especial, com uma personalidade única e capacidade de socialização de dar inveja.<br> Ao mesmo tempo que me arrependo de ter trocado de escola, penso que, se eu não tivesse trocado, eu nunca teria te conhecido, então não foi algo ruim (gosto muito de pensar assim). Queria desejar um Feliz Aniversário pra você e uma vida íncrivel. <br> Espero podermos passar mais tempo juntos nessas férias <br><br><br><br><br><br><br><br> Não sei se o texto ficou bom, mas eu fiz de coração e usando minhas básicas habilidades de programação.<br>Ass. Caio<br><br><button class='botaotexto'><a href='./index.html'><--<a></button></div>"
+}
